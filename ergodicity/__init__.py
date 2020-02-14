dd538f14c8f797981a2db9d5b8e00d42d14172b4
Gustavo Landfried <gustavolandfried@gmail.com>
2020-02-14 13:55:00 -0300

    Add the free rider simulation

--- ergodicity/__init__.py
+++ ergodicity/__init__.py
@@ -117,7 +117,31 @@ def walk_incest(iteratons,n_communities,incest=1):
     for i in range(iteratons):
         history.append(list(map(lambda x: simple_gamble(x), incest_rule(history[-1],incest) ) ))
     return history
+
+
+def incest_rule_free_rider(communities_size,free_rider,exogamy=0.05):
+    res = []
+    n_free_riders = len(free_rider)
+    size_not_free_rider = sum(map(lambda i: 0 if i in free_rider else communities_size[i], range(len(communities_size))))
+    migration_per_community = exogamy*size_not_free_rider/len(communities_size)
+    for c in range(len(communities_size)):#c=0
+        res.append( communities_size[c]*(1-exogamy*int(not c in free_rider)) + migration_per_community)
+    return res
+
+def walk_incest_free_rider(iteratons,n_communities,n_free_riders=1,incest=1):
+    communities = init_communities(n_communities)
+    free_riders = list(range(n_free_riders))
+    history = []
+    history.append(communities)
+    for i in range(iteratons):
+        history.append(list(map(lambda x: simple_gamble(x), incest_rule_free_rider(history[-1],free_riders,incest) ) ))
+    return history
  
+
+"""
+Share resource, but play you one game.
+"""    
+
 def walk_sharing(iteratons=1000,n_communities=150):
     communities = init_communities(n_communities)
     history = []
